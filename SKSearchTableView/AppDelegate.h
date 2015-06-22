29512a19a2c2f42fcfa7166f20b3acc505588686
satish kumar <satishkumara225@gmail.com>
2015-06-22 12:18:12 +0530

    Initial Commit

---
+++ SKSearchTableView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SKSearchTableView
+//
+//  Created by satheesh on 6/22/15.
+//  Copyright (c) 2015 Satish. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
